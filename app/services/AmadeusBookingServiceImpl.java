39f94671283f7dcbb9a9e4884fa4f0fb0e73f7d9
sathishauit1 <a.satish@spekond.com>
2025-10-15 13:29:58 +0530

    QA deployment (#613)
    * split ticket fare rules for amadeus
    
    * split ticket issues
    
    * Indigo Seaman Fares (#525)
    
    * Reissue Search not working fix (#564)
    
    * Indigo reissue impl (#566)
    
    * Reissue Search for Indigo
    
    * Removed file commit
    
    * baggage and fare rules (#568)
    
    * baggage and fare rules
    
    * fare rules issue
    
    ---------
    
    Co-authored-by: sathishauit1 <satish.a@spekond.com>
    
    * Split tkt (#570)
    
    * baggage and fare rules
    
    * fare rules issue
    
    * quick fix for booking issue
    
    * booking issue
    
    ---------
    
    Co-authored-by: sathishauit1 <satish.a@spekond.com>
    
    * Split tkt (#572)
    
    * baggage and fare rules
    
    * fare rules issue
    
    * quick fix for booking issue
    
    * booking issue
    
    * cabin details and mnr baggage
    
    ---------
    
    Co-authored-by: sathishauit1 <satish.a@spekond.com>
    
    * indigo full refund
    
    * booking refund indigo
    
    * split ticket issues (#575)
    
    Co-authored-by: sathishauit1 <satish.a@spekond.com>
    
    * Merge pull request #576 from spekond-tech/FareRules-ptc
    
    Added PTC for fare check rules rsponse
    
    * split ticket search optimisation (#577)
    
    Co-authored-by: sathishauit1 <satish.a@spekond.com>
    
    * Split search opti (#579)
    
    * split ticket search optimisation
    
    * split ticket multi threaded search
    
    ---------
    
    Co-authored-by: sathishauit1 <satish.a@spekond.com>
    
    * partial refund indigo and full refund issues (#581)
    
    Co-authored-by: sathishauit1 <satish.a@spekond.com>
    
    * upload booking pnr pricing converting it into inr (#584)
    
    Co-authored-by: Chetan-Spekond <Chetan-Spekond>
    
    * Ow rt buffering (#583)
    
    * Added timeout of 30 seconds for Indigo responses
    
    * ...
    
    * split ticket fare issues
    
    * split ticket price change pnr (#589)
    
    Co-authored-by: sathishauit1 <satish.a@spekond.com>
    
    * addBooking benzy officeId bug fixes (#588)
    
    * removed one thread from search
    
    * split ticket issue (#593)
    
    Co-authored-by: sathishauit1 <satish.a@spekond.com>
    
    * upload booking rePricing fix (#595)
    
    Co-authored-by: Chetan-Spekond <Chetan-Spekond>
    
    * Akbar travels impl (#598)
    
    * Akbar Travels Search Set Up
    
    * ...
    
    * Akbar One Way, Grouping of fares and Round trip Search completed
    
    * Baggage meals fix for add booking (#600)
    
    * baggage and meals issue fix
    
    * baggage and meals fix
    
    ---------
    
    Co-authored-by: Chetan-Spekond <Chetan-Spekond>
    
    * void and refund bug fix (#603)
    
    * Akbar baggage and fare impl (#606)
    
    * Baggage and Pricing information completed
    
    * Baggage and fare rule fix
    
    * Reissue array check (#611)
    
    * upload ticket issue in production
    
    ---------
    
    Co-authored-by: sathishauit1 <satish.a@spekond.com>
    Co-authored-by: nithinspekond <150015436+nithinspekond@users.noreply.github.com>
    Co-authored-by: Shilpa_A <a.shilpa@spekond.com>
    Co-authored-by: Chetan-Spekond <k.chetan@spekond.com>
    Co-authored-by: Tanuja-spekond <s.tanuja@spekond.com>

--- app/services/AmadeusBookingServiceImpl.java
+++ app/services/AmadeusBookingServiceImpl.java
@@ -2104,6 +2104,7 @@ public class AmadeusBookingServiceImpl implements BookingService {
 
     public JsonNode getBookingDetailsByOfficeId(String gdsPNR, String officeId) {
         logger.info("Amadeus getBookingDetailsByOfficeId called for PNR: " + gdsPNR + "--OfficeID: " + officeId);
+        officeId = amadeusSourceOfficeService.getDelhiSourceOffice().getOfficeId();
         AmadeusSessionWrapper sessionWrapper = serviceHandler.logIn(officeId, true);
         return getBookingDetail(gdsPNR, sessionWrapper);
     }
